e28b1c4a9c032e56f4a1bea7d0ee7d0f15d4d883
Doublemine <Doublemine.w@gmail.com>
2018-10-01 18:30:46 +0800

    Refactor: move to docker

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 set -ex \
-&& python fetch.py \
+&& docker run --name fetch -v "${PWD}:/app" -e GIT_REPO=${GIT_REPO}  doublemine/helm-chart-fetcher:3.7\
 && git add . \
 && git commit -am "travis automated update helm charts " \
 && git checkout -B master \
